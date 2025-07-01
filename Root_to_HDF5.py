d1ef9239728766dc75f5a10bf613a4c7886dcc80
ereinha <eric0reinhardt@gmail.com>
2025-07-01 10:50:23 -0500

    Update Root_to_HDF5.py

--- Root_to_HDF5.py
+++ Root_to_HDF5.py
@@ -10,7 +10,6 @@ parser.add_argument('-o', '--outdir', default='.', type=str, help='Output pq fil
 parser.add_argument('-d', '--decay', default='test', type=str, help='Decay name.')
 parser.add_argument('-n', '--idx', default=0, type=int, help='Input root file index.')
 parser.add_argument('-c', '--chunk_size', default=32, type=int, help='chunk size for h5 file')
-parser.add_argument('-m', '--mass', default='', type=str, help='signal mass')
 args = parser.parse_args()
 chunk_size = args.chunk_size
 
@@ -75,120 +74,129 @@ print(" >> Processing entries: [",iEvtStart,"->",iEvtEnd,")")
 
 sw = ROOT.TStopwatch()
 sw.Start()
-with h5py.File(f'{outStr}', 'w') as proper_data:
-        dataset_names = ['A_diphoton_gen_m0', 'A_diphoton_gen_dR', 'A_diphoton_gen_E', 
-                         'A_diphoton_gen_pT', 'A_diphoton_gen_eta', 'A_diphoton_gen_phi',
-                         'A_diphoton_reco_M', 'A_diphoton_reco_dR', 'A_diphoton_reco_E',
-                         'A_diphoton_reco_pT', 'A_diphoton_reco_eta', 'A_diphoton_reco_phi',
-                         'A_ditau_gen_m0', 'A_ditau_gen_dR', 'A_ditau_gen_E',
-                         'A_ditau_gen_pT', 'A_ditau_gen_eta', 'A_ditau_gen_phi']
-        datasets = {
-            name: proper_data.create_dataset(
+with h5py.File(outStr, "w") as proper_data:
+
+    # geometry-dependent constants
+    HIT_SHAPE   = (1500, 11, 3)           # one row ->  zero-suppressed hit matrix
+    SCALAR_SHAPE = (1,)                   # one row ->  any single scalar value
+
+    dataset_names = [
+        'A_diphoton_gen_m0',  'A_diphoton_gen_dR',  'A_diphoton_gen_E',
+        'A_diphoton_gen_pT',  'A_diphoton_gen_eta', 'A_diphoton_gen_phi',
+        'A_diphoton_reco_M',  'A_diphoton_reco_dR', 'A_diphoton_reco_E',
+        'A_diphoton_reco_pT', 'A_diphoton_reco_eta','A_diphoton_reco_phi',
+        'A_ditau_gen_m0',     'A_ditau_gen_dR',     'A_ditau_gen_E',
+        'A_ditau_gen_pT',     'A_ditau_gen_eta',    'A_ditau_gen_phi',
+        'zero_suppressed_hit_collection'
+    ]
+
+    dsets = {}
+    for name in dataset_names:
+        if name == 'zero_suppressed_hit_collection':
+            dsets[name] = proper_data.create_dataset(
                 name,
-                shape= (0, 1500, 11, 3) if 'zero_suppressed_hit_collection' in name else (0,1),
-                maxshape=(None, 1500, 11, 3) if 'zero_suppressed_hit_collection' in name else (None, 1),
-                dtype='float32',  # Specify an appropriate data type
+                shape=(nEvts, *HIT_SHAPE),
+                dtype='float32',
                 compression='lzf',
-                chunks=(chunk_size, 1500, 11, 3) if 'zero_suppressed_hit_collection' in name else (chunk_size, 1),
-            ) for name in dataset_names
-        }
-        end_idx = 0
-        for iEvt in range(iEvtStart,iEvtEnd):
-
-            # Initialize event
-            rhTree.GetEntry(iEvt)
-
-            if iEvt % 100 == 0:
-                print(" .. Processing entry",iEvt)
-
-            # Jet attributes
-            A_diphoton_gen_m0       = rhTree.A_diphoton_gen_m0
-            A_diphoton_gen_dR       = rhTree.A_diphoton_gen_dR
-            A_diphoton_gen_E        = rhTree.A_diphoton_gen_E
-            A_diphoton_gen_pT       = rhTree.A_diphoton_gen_pT
-            A_diphoton_gen_eta      = rhTree.A_diphoton_gen_eta
-            A_diphoton_gen_phi      = rhTree.A_diphoton_gen_phi
-            A_diphoton_reco_M       = rhTree.A_diphoton_reco_M
-            A_diphoton_reco_dR      = rhTree.A_diphoton_reco_dR
-            A_diphoton_reco_E       = rhTree.A_diphoton_reco_E
-            A_diphoton_reco_pT      = rhTree.A_diphoton_reco_pT
-            A_diphoton_reco_eta     = rhTree.A_diphoton_reco_eta
-            A_diphoton_reco_phi     = rhTree.A_diphoton_reco_phi
-
-            A_ditau_gen_m0          = rhTree.A_ditau_gen_m0
-            A_ditau_gen_dR          = rhTree.A_ditau_gen_dR
-            A_ditau_gen_E           = rhTree.A_ditau_gen_E
-            A_ditau_gen_pT          = rhTree.A_ditau_gen_pT
-            A_ditau_gen_eta         = rhTree.A_ditau_gen_eta
-            A_ditau_gen_phi         = rhTree.A_ditau_gen_phi
-
-            ys  = len(A_diphoton_gen_m0)
-            if ys < 2: continue
-            end_idx = end_idx + ys
+                chunks=(args.chunk_size, *HIT_SHAPE)   # keep fast chunking on disk
+            )
+        else:
+            dsets[name] = proper_data.create_dataset(
+                name,
+                shape=(nEvts, *SCALAR_SHAPE),
+                dtype='float32',
+                compression='lzf',
+                chunks=(args.chunk_size, *SCALAR_SHAPE)
+            )
 
 
+    BUFFER_ROWS      = args.chunk_size * 32     # any multiple of chunk size works
+    hit_buffer       = np.empty((BUFFER_ROWS, *HIT_SHAPE),   dtype=np.float32)
+    scalar_buffers   = {n: np.empty((BUFFER_ROWS, 1), dtype=np.float32)
+                        for n in dataset_names if n != 'zero_suppressed_hit_collection'}
 
-            HBHE_energy        = np.array(rhTree.HBHE_energy).reshape(56*72)
-            ECAL_energy        = np.array(rhTree.ECAL_energy).reshape(280*360)
-            TracksAtECAL_pt    = np.array(rhTree.ECAL_tracksPt_atECALfixIP).reshape(280*360)
-            PixAtEcal_1        = np.array(rhTree.BPIX_layer1_ECAL_atPV).reshape(280*360)
-            PixAtEcal_2        = np.array(rhTree.BPIX_layer2_ECAL_atPV).reshape(280*360)
-            PixAtEcal_3        = np.array(rhTree.BPIX_layer3_ECAL_atPV).reshape(280*360)
-            PixAtEcal_4        = np.array(rhTree.BPIX_layer4_ECAL_atPV).reshape(280*360)
-            TibAtEcal_1        = np.array(rhTree.TIB_layer1_ECAL_atPV).reshape(280*360)
-            TibAtEcal_2        = np.array(rhTree.TIB_layer2_ECAL_atPV).reshape(280*360)
-            TobAtEcal_1        = np.array(rhTree.TOB_layer1_ECAL_atPV).reshape(280*360)
-            TobAtEcal_2        = np.array(rhTree.TOB_layer2_ECAL_atPV).reshape(280*360)
-            HighResCollection            = np.stack([TracksAtECAL_pt, ECAL_energy, PixAtEcal_1, 
-                                                     PixAtEcal_2, PixAtEcal_3, PixAtEcal_4, 
-                                                     TibAtEcal_1, TibAtEcal_2, TobAtEcal_1, 
-                                                     TobAtEcal_2], axis=1)
+    write_head = 0 # next row in HDF5 file to be filled
+    buf_fill   = 0 # rows currently stored in the in-memory buffer
 
-            for name, dataset in datasets.items():
-                dataset.resize((end_idx, 1500, 11, 3) if 'zero_suppressed_hit_collection' in name else (end_idx,1))
+    for iEvt in range(iEvtStart, iEvtEnd):
 
-            # build hit matrix once per event (will be reused for each jet i)
-            hit_matrix = np.full((1500, 11, 3), -np.inf, dtype=np.float32)
+        rhTree.GetEntry(iEvt)
+        if iEvt % 100 == 0:
+            print(" .. Processing entry", iEvt)
 
-            # 10 high-res channels (use HCAL grid for coords)
-            for ch in range(10):
-                hit_slice, over = compress_channel(
-                    HighResCollection[:, ch],
-                    HCAL_etaphi, 1500, 1e-4)
-                hit_matrix[:, ch, :] = hit_slice
-                if over:
-                    overflow_counter[ch] += 1
+        HighResCollection = np.stack([
+            np.array(rhTree.ECAL_tracksPt_atECALfixIP).reshape(280*360),
+            np.array(rhTree.ECAL_energy).reshape(280*360),
+            np.array(rhTree.BPIX_layer1_ECAL_atPV).reshape(280*360),
+            np.array(rhTree.BPIX_layer2_ECAL_atPV).reshape(280*360),
+            np.array(rhTree.BPIX_layer3_ECAL_atPV).reshape(280*360),
+            np.array(rhTree.BPIX_layer4_ECAL_atPV).reshape(280*360),
+            np.array(rhTree.TIB_layer1_ECAL_atPV).reshape(280*360),
+            np.array(rhTree.TIB_layer2_ECAL_atPV).reshape(280*360),
+            np.array(rhTree.TOB_layer1_ECAL_atPV).reshape(280*360),
+            np.array(rhTree.TOB_layer2_ECAL_atPV).reshape(280*360)
+        ], axis=1)
 
-            # HBHE channel (use ECAL grid for coords)
-            hit_slice, over = compress_channel(
-                HBHE_energy,
-                ECAL_etaphi, 1500, 1e-4)
-            hit_matrix[:, 10, :] = hit_slice
+        hit_matrix = np.full((1500, 11, 3), -np.inf, dtype=np.float32)
+
+        # 10 high-resolution channels (HCAL grid for coordinates)
+        for ch in range(10):
+            hit_slice, over = compress_channel(HighResCollection[:, ch], HCAL_etaphi, 1500, 1e-4)
+            hit_matrix[:, ch, :] = hit_slice
             if over:
-                overflow_counter[10] += 1
+                overflow_counter[ch] += 1
 
-            for i in range(ys):
-                proper_data['zero_suppressed_hit_collection'][end_idx - ys + i, :, :, :] = hit_matrix
+        # HBHE channel (ECAL grid for coordinates)
+        HBHE_energy = np.array(rhTree.HBHE_energy).reshape(56*72)
+        hit_slice, over = compress_channel(HBHE_energy, ECAL_etaphi, 1500, 1e-4)
+        hit_matrix[:, 10, :] = hit_slice
+        if over:
+            overflow_counter[10] += 1
 
-                proper_data['A_diphoton_gen_m0'][end_idx - ys + i, :]   = A_diphoton_gen_m0[i]
-                proper_data['A_diphoton_gen_dR'][end_idx - ys + i, :]   = A_diphoton_gen_dR[i]
-                proper_data['A_diphoton_gen_E'][end_idx - ys + i, :]    = A_diphoton_gen_E[i]
-                proper_data['A_diphoton_gen_pT'][end_idx - ys + i, :]   = A_diphoton_gen_pT[i]
-                proper_data['A_diphoton_gen_eta'][end_idx - ys + i, :]  = A_diphoton_gen_eta[i]
-                proper_data['A_diphoton_gen_phi'][end_idx - ys + i, :]  = A_diphoton_gen_phi[i]
-                proper_data['A_diphoton_reco_M'][end_idx - ys + i, :]   = A_diphoton_reco_M[i]
-                proper_data['A_diphoton_reco_dR'][end_idx - ys + i, :]  = A_diphoton_reco_dR[i]
-                proper_data['A_diphoton_reco_E'][end_idx - ys + i, :]   = A_diphoton_reco_E[i]
-                proper_data['A_diphoton_reco_pT'][end_idx - ys + i, :]  = A_diphoton_reco_pT[i]
-                proper_data['A_diphoton_reco_eta'][end_idx - ys + i, :] = A_diphoton_reco_eta[i]
-                proper_data['A_diphoton_reco_phi'][end_idx - ys + i, :] = A_diphoton_reco_phi[i]
 
-                proper_data['A_ditau_gen_m0'][end_idx - ys + i, :]      = A_ditau_gen_m0[i]
-                proper_data['A_ditau_gen_dR'][end_idx - ys + i, :]      = A_ditau_gen_dR[i]
-                proper_data['A_ditau_gen_E'][end_idx - ys + i, :]       = A_ditau_gen_E[i]
-                proper_data['A_ditau_gen_pT'][end_idx - ys + i, :]      = A_ditau_gen_pT[i]
-                proper_data['A_ditau_gen_eta'][end_idx - ys + i, :]     = A_ditau_gen_eta[i]
-                proper_data['A_ditau_gen_phi'][end_idx - ys + i, :]     = A_ditau_gen_phi[i]
+        # fill the in-RAM buffer
+        hit_buffer[buf_fill, :, :, :] = hit_matrix
+        scalar_buffers['A_diphoton_gen_m0' ][buf_fill, 0] = rhTree.A_diphoton_gen_m0[0]
+        scalar_buffers['A_diphoton_gen_dR' ][buf_fill, 0] = rhTree.A_diphoton_gen_dR[0]
+        scalar_buffers['A_diphoton_gen_E'  ][buf_fill, 0] = rhTree.A_diphoton_gen_E[0]
+        scalar_buffers['A_diphoton_gen_pT' ][buf_fill, 0] = rhTree.A_diphoton_gen_pT[0]
+        scalar_buffers['A_diphoton_gen_eta'][buf_fill, 0] = rhTree.A_diphoton_gen_eta[0]
+        scalar_buffers['A_diphoton_gen_phi'][buf_fill, 0] = rhTree.A_diphoton_gen_phi[0]
+
+        scalar_buffers['A_diphoton_reco_M' ][buf_fill, 0] = rhTree.A_diphoton_reco_M[0]
+        scalar_buffers['A_diphoton_reco_dR'][buf_fill, 0] = rhTree.A_diphoton_reco_dR[0]
+        scalar_buffers['A_diphoton_reco_E' ][buf_fill, 0] = rhTree.A_diphoton_reco_E[0]
+        scalar_buffers['A_diphoton_reco_pT'][buf_fill, 0] = rhTree.A_diphoton_reco_pT[0]
+        scalar_buffers['A_diphoton_reco_eta'][buf_fill, 0] = rhTree.A_diphoton_reco_eta[0]
+        scalar_buffers['A_diphoton_reco_phi'][buf_fill, 0] = rhTree.A_diphoton_reco_phi[0]
+
+        scalar_buffers['A_ditau_gen_m0'    ][buf_fill, 0] = rhTree.A_ditau_gen_m0[0]
+        scalar_buffers['A_ditau_gen_dR'    ][buf_fill, 0] = rhTree.A_ditau_gen_dR[0]
+        scalar_buffers['A_ditau_gen_E'     ][buf_fill, 0] = rhTree.A_ditau_gen_E[0]
+        scalar_buffers['A_ditau_gen_pT'    ][buf_fill, 0] = rhTree.A_ditau_gen_pT[0]
+        scalar_buffers['A_ditau_gen_eta'   ][buf_fill, 0] = rhTree.A_ditau_gen_eta[0]
+        scalar_buffers['A_ditau_gen_phi'   ][buf_fill, 0] = rhTree.A_ditau_gen_phi[0]
+
+        buf_fill += 1
+
+        if buf_fill == BUFFER_ROWS:
+            slice_ = slice(write_head, write_head + buf_fill)
+            dsets['zero_suppressed_hit_collection'][slice_, :, :, :] = hit_buffer
+            for n, arr in scalar_buffers.items():
+                dsets[n][slice_, :] = arr
+
+            write_head += buf_fill
+            buf_fill = 0      # reset buffer pointer
+
+    if buf_fill:
+        slice_ = slice(write_head, write_head + buf_fill)
+        dsets['zero_suppressed_hit_collection'][slice_, :, :, :] = hit_buffer[:buf_fill]
+        for n, arr in scalar_buffers.items():
+            dsets[n][slice_, :] = arr[:buf_fill]
+
+        write_head += buf_fill   # (optional) sanity check
+
+    assert write_head == nEvts, "Logic error - not all rows written!"
 
 print(" >> Real time:", sw.RealTime() / 60., "minutes")
 print(" >> CPU time: ", sw.CpuTime() / 60., "minutes")
